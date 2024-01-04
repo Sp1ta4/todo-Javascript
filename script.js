454d45399a869c4eab4cd969957d954ee71d4e10
Glak Gevorgyan <sp1tak.gg@gmail.com>
2024-01-05 00:55:03 +0400

    Добавил изменение таска через инпут. Исправил несколько багов связанных с анимациями кнопок. Начал делать пагинацию

--- script.js
+++ script.js
@@ -2,26 +2,50 @@ const $ = document;
 const tasksDiv = $.querySelector('#tasks-div')
 const tasks = $.getElementsByClassName('.task');
 const addInput = $.querySelector('#formGroupExampleInput');
+const pagLinks = $.querySelectorAll('.pagination .page-link');
 let tasksData;
+const pageMaxTasksCount = 5;
+for (const link of pagLinks) {
+  link.onclick = () => {
+    let currentPage = +link.innerHTML;
+    let start = (currentPage - 1) * pageMaxTasksCount;
+    let end = start + pageMaxTasksCount;
+    const notes = tasksData.slice(start, end);
+    console.log(notes);
+  }
+}
 
 window.addEventListener('load', () => {
-  console.log('lox');
   tasksData = JSON.parse(localStorage.getItem("todos")) || [];
-  tasksData.length && renderTasks(tasksData);
+  renderTasks(tasksData.slice(0, pageMaxTasksCount));
 });
 
 addInput.addEventListener('keydown', function (e) {
   if (e.keyCode === 13) {
     const newTask = { id: _.uniqueId(), task: addInput.value, completed: false };
+    console.log(newTask);
     tasksData.push(newTask);
     localStorage.setItem("todos", JSON.stringify(tasksData));
     addInput.value = '';
+    if (tasksData.length === 1) {
+      tasksDiv.innerHTML = '';
+    }
     tasksDiv.appendChild(createElement(newTask));
   }
 })
 
 function renderTasks(tasksArray) {
-  tasksArray.forEach(elem => tasksDiv.appendChild(createElement(elem)))
+  tasksArray.length ? tasksArray.forEach(elem => {
+    tasksDiv.appendChild(createElement(elem));
+  }) : addClearList()
+}
+
+function addClearList() {
+  tasksDiv.innerHTML = `
+            <div class="d-flex flex-column justify-content-center align-items-center" style="min-height: 290px !important;">
+                <img src="./images/empty.png" alt="empty" width="100" height="100">
+                <h5 class="text-secondary">You have not added any tasks yet</h5>
+            </div>`
 }
 
 function updateDeleted(taskElement) {
@@ -30,22 +54,23 @@ function updateDeleted(taskElement) {
 
 function updateCompleted(taskElement, obj) {
   taskElement.innerHTML = `
-  <div class="checkbox-wrapper-52 p-3 ps-3 d-flex">
-            <div class="item d-flex align-items-center">
-                ${obj.completed ? `<svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="22" height="22" viewBox="0 0 24 24" fill="#18CDA6" onmousedown="toggleCompleted(${obj.id})" class="btn btn-link p-0 m-0">
-                    <path d="M 20.292969 5.2929688 L 9 16.585938 L 4.7070312 12.292969 L 3.2929688 13.707031 L 9 19.414062 L 21.707031 6.7070312 L 20.292969 5.2929688 z"></path>
-                    </svg>`
+    <div class="checkbox-wrapper-52 p-3 ps-3 d-flex" >
+      <div class="item d-flex align-items-center">
+        ${obj.completed ?
+      `<svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="22" height="22" viewBox="0 0 24 24" fill="#18CDA6" onmousedown="toggleCompleted(${obj.id})" class="btn btn-link p-0 m-0">
+            <path d="M 20.292969 5.2929688 L 9 16.585938 L 4.7070312 12.292969 L 3.2929688 13.707031 L 9 19.414062 L 21.707031 6.7070312 L 20.292969 5.2929688  z"></path>
+          </svg>`
       : `<div class="btn border rounded p-0 m-0" style="width:22px; height:22px" onmousedown="toggleCompleted(${obj.id})"></div>`}
-            </div>
-            <span class="${obj.completed ? "cbx-lbl fs-3 ms-3 text-muted text-decoration-line-through" : "cbx-lbl fs-3 ms-3"}">${obj.task}</span>
-        </div >
-    <div class="buttons position-absolute end-0 d-flex mt-1 me-4 p-2 ps-3 pe-3">
-      <div class="button edit-btn">
+      </div>
+      <span class="${obj.completed ? "cbx-lbl ms-3 text-muted text-decoration-line-through" : "cbx-lbl ms-3"}">${obj.task}</span>
+    </div>
+    <div class="buttons position-absolute end-0 d-flex justify-content-center align-items-center mt-1 me-4 p-2 ps-3 pe-3">
+      <div class="button d-flex justify-content-center align-items-center edit-btn" onclick="editTask(${obj.id})">
         <span class="material-symbols-outlined">
           edit
         </span>
       </div>
-      <div class="button ms-4 delete-btn" onclick="deleteTask(${obj.id})">
+      <div class="button d-flex justify-content-center align-items-center ms-4 delete-btn" onclick="deleteTask(${obj.id})">
         <span class="material-symbols-outlined">
           delete
         </span>
@@ -58,19 +83,23 @@ function createElement(obj) {
   task.className = 'task d-flex align-items-center position-relative';
   task.id = obj.id;
   task.innerHTML = `
-      <div class="checkbox-wrapper-52 p-3 ps-3 d-flex" >
-            <div  class="item d-flex align-items-center">
-                <div class="btn border rounded p-0 m-0" style="width:22px; height:22px" onmousedown="toggleCompleted(${obj.id})"></div>
-            </div>
-            <span class="cbx-lbl fs-3 ms-3">${obj.task}</span>
+      <div class="checkbox-wrapper-52 p-3 ps-3 d-flex">
+            <div class="item d-flex align-items-center">
+        ${obj.completed ?
+      `<svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="22" height="22" viewBox="0 0 24 24" fill="#18CDA6" onmousedown="toggleCompleted(${obj.id})" class="btn btn-link p-0 m-0">
+            <path d="M 20.292969 5.2929688 L 9 16.585938 L 4.7070312 12.292969 L 3.2929688 13.707031 L 9 19.414062 L 21.707031 6.7070312 L 20.292969 5.2929688  z"></path>
+          </svg>`
+      : `<div class="btn border rounded p-0 m-0" style="width:22px; height:22px" onmousedown="toggleCompleted(${obj.id})"></div>`}
+      </div>
+            <span class="cbx-lbl ms-3">${obj.task}</span>
         </div>
-    <div class="buttons position-absolute end-0 d-flex mt-1 me-4 p-2 ps-3 pe-3">
-      <div class="button edit-btn">
+    <div class="buttons position-absolute end-0 d-flex justify-content-center align-items-center mt-1 me-4 p-2 ps-3 pe-3">
+      <div class="button d-flex justify-content-center align-items-center edit-btn" onclick="editTask(${obj.id})">
         <span class="material-symbols-outlined">
           edit
         </span>
       </div>
-      <div class="button ms-4 delete-btn" onclick="deleteTask(${obj.id})">
+      <div class="button d-flex justify-content-center align-items-center ms-4 delete-btn" onclick="deleteTask(${obj.id})">
         <span class="material-symbols-outlined">
           delete
         </span>
@@ -90,4 +119,44 @@ function deleteTask(id) {
   const taskElement = document.getElementById(id);
   localStorage.setItem("todos", JSON.stringify(tasksData));
   taskElement.parentNode.removeChild(taskElement);
+  !tasksData.length && addClearList();
+}
+function updateTask(task, id) {
+  const updatedTask = tasksData.find(elem => +elem.id === id);
+  tasksData.map(elem => +elem.id === id ? elem.task = task : elem);
+  localStorage.setItem("todos", JSON.stringify(tasksData));
+  updateCompleted(document.getElementById(updatedTask.id), updatedTask);
+}
+function editTask(id) {
+  const updatedTask = tasksData.find(elem => +elem.id === id);
+  const taskElement = document.getElementById(id);
+  taskElement.innerHTML = `<div class="checkbox-wrapper-52 p-3 ps-3 d-flex">
+            <div class="item d-flex align-items-center">
+        ${updatedTask.completed ?
+      `<svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="22" height="22" viewBox="0 0 24 24" fill="#18CDA6" onmousedown="toggleCompleted(${updatedTask.id})" class="btn btn-link p-0 m-0">
+            <path d="M 20.292969 5.2929688 L 9 16.585938 L 4.7070312 12.292969 L 3.2929688 13.707031 L 9 19.414062 L 21.707031 6.7070312 L 20.292969 5.2929688  z"></path>
+          </svg>`
+      : `<div class="btn border rounded p-0 m-0" style="width:22px; height:22px" onmousedown="toggleCompleted(${updatedTask.id})"></div>`}
+      </div>
+            <div class="editDiv d-flex">
+              <input type="text" class="form-control ms-3" id="formGroupExampleInput" value="${updatedTask.task}" maxlength="43">
+              <div class="btn btn-link p-2" onclick="(() => updateTask(this.parentNode.firstElementChild.value, ${id}))()">
+                <span class="material-symbols-outlined">
+                  done
+                </span>
+              </div>
+            </div>
+        </div>
+    <div class="buttons position-absolute end-0 d-flex justify-content-center align-items-center mt-1 me-4 p-2 ps-3 pe-3">
+      <div class="button d-flex justify-content-center align-items-center edit-btn" onclick="editTask(${id})">
+        <span class="material-symbols-outlined">
+          edit
+        </span>
+      </div>
+      <div class="button d-flex justify-content-center align-items-center ms-4 delete-btn" onclick="deleteTask(${id})">
+        <span class="material-symbols-outlined">
+          delete
+        </span>
+      </div>
+    </div>`;
 }
