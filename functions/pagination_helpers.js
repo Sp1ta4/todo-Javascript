86f2eaabab4d137247e97dce6ecaf185f83d32e4
Glak Gevorgyan <sp1tak.gg@gmail.com>
2024-01-15 00:01:44 +0400

    отрефакторил половину пагинции

--- functions/pagination_helpers.js
+++ functions/pagination_helpers.js
@@ -1,41 +1,30 @@
-// function addPaginationLink(data, pagination, pageMaxTasksCount) {
-//   pagination.innerHTML = '';
-//   const linksCount = Math.ceil(data.length / pageMaxTasksCount);
-//   for (let i = 1; i <= linksCount; i++) {
-//     pagination.insertAdjacentHTML('beforeend',
-//       `<li class="page-item d-flex justify-content-center align-items-center">
-//        <button class="btn btn-link page-link ps-3 pe-3 p-1 fs-5 ${i === linksCount && 'activeLink'}">${i}</button>
-//       </li>`);
-//   }
-//   const notes = listCalculation(data, linksCount, pageMaxTasksCount);
-//   paginationNexPrev(notes, linksCount);
+function addLinkElementsInPagination(data, pageMaxTasksCount) {
+  const linksCount = Math.ceil(data.length / pageMaxTasksCount);
+  let paginationElements = ''
+  for (let i = 1; i <= linksCount; i++) {
+    paginationElements += `<li class="page-item d-flex justify-content-center align-items-center">
+    <button class="btn btn-link page-link ps-3 pe-3 p-1 fs-5" onclick="paginationLinksFunction(this)" id="${i}">${i}</button>
+    </li>`;
+  }
+  pagination.innerHTML = paginationElements;
+  const notes = listCalculation(data, linksCount, pageMaxTasksCount);
+  renderTasks(notes);
+}
+function paginationLinksFunction(self) {
+  let clickedPage = +self.innerHTML;
+  const notes = listCalculation(tasksData, clickedPage, pageMaxTasksCount);
+  renderTasks(notes);
+  addActiveStateClassToPaginationLinks(self.id);
+}
+// function addActiveStateClassToPaginationLinks(id) {
+//   debugger
+//   const paginationLinksElements = document.querySelectorAll('.pagination .page-link');
+//   [...paginationLinksElements].map(elem => {
+//     return elem.id === id ? elem.classList.add('activeLink') : elem.classList.remove('activeLink');
+//   });
 // }
-// function paginationLinkAddClick(tasksData, pageMaxTasksCount) {
-//   const pageLinks = document.querySelectorAll('.pagination .page-link');
-//   for (const link of pageLinks) {
-//     link.onclick = function (event) {
-//       let clickedPage = +event.target.innerHTML;
-//       const notes = listCalculation(tasksData, clickedPage, pageMaxTasksCount);
-//       paginationNexPrev(notes, clickedPage);
-//       pageLinks.forEach(elem => {
-//         elem === link ? elem.classList.add('activeLink') : elem.classList.remove('activeLink')
-//       });
-//     }
-//   }
-// }
-// function listCalculation(tasksData, currentPage, pageMaxTasksCount) {
-//   let start = (currentPage - 1) * pageMaxTasksCount;
-//   let end = start + pageMaxTasksCount;
-//   return tasksData.slice(start, end);
-// }
-// function paginationNexPrev(data, clickedPage) {
-//   const tasksDiv = document.querySelector('#tasks-div');
-//   const currentPage = document.querySelector('.activeLink');
-//   if (+currentPage.innerHTML !== clickedPage) {
-//     tasksDiv.innerHTML = '';
-//     for (task of data) {
-//       tasksDiv.appendChild(createTaskElement(data, task))
-//     }
-//   }
-
-// }
+function listCalculation(tasksData, currentPage, pageMaxTasksCount) {
+  let start = (currentPage - 1) * pageMaxTasksCount;
+  let end = start + pageMaxTasksCount;
+  return tasksData.slice(start, end);
+}
--- script.js
+++ script.js
@@ -1,23 +1,27 @@
-const $ = document;
-const tasksDiv = $.querySelector('#tasks-div');
-const tasks = $.getElementsByClassName('.task');
-const addInput = $.querySelector('#formGroupExampleInput');
-const pagination = $.querySelector('.pagination');
+const tasksDiv = document.querySelector('#tasks-div');
+const tasks = document.getElementsByClassName('.task');
+const addInput = document.querySelector('#formGroupExampleInput');
+const pagination = document.querySelector('.pagination');
 const pageMaxTasksCount = 5;
 let taskId = 0;
 let tasksData;
 
+//Start of application
 updateData();
 taskId = updateID(taskId);
 renderTasks(tasksData.slice(0, pageMaxTasksCount));
-console.log(taskId);
+
 addInput.addEventListener('keydown', function (e) {
   if (e.keyCode === 13 && addInput.value) {
     const newTask = { id: taskId, task: addInput.value, completed: false };
     taskId++;
     tasksData.push(newTask);
     postData(tasksData);
-    renderTasks(tasksData.slice(0, pageMaxTasksCount));
+    if (tasksData.length > pageMaxTasksCount) {
+      addLinkElementsInPagination(tasksData, pageMaxTasksCount);
+    } else {
+      renderTasks(tasksData.slice(0, pageMaxTasksCount));
+    }
     addInput.value = '';
   }
 })
